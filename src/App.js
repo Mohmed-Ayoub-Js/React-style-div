b60d32b063240ffe48e30754056f04e0a68d914b
MohmedAyoubJs <113200512+MohmedAyoubProgrammer@users.noreply.github.com>
2023-05-15 17:35:33 +0100

    code
    This code uses React and CSS to create a circular div element. It applies styling for background color, text color, border radius, and transition effects. When hovering over the div, a shadow effect and color change occur. The page has a dark gray background.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+  <center>
+    <div class="app"> 
+      <center> 
+        <h1>
+         Div  
+        </h1>
+      </center>
+    </div>
+  </center>
+  );
+}
